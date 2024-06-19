19432f7c505ef0abc3e498764f20bc4cb8ecb2c3
SurajjBhardwaj <pandeyyysuraj@gmail.com>
2024-06-19 16:38:17 +0530

    working perfectly

---
+++ src/popup/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Auto Scroll Extension</h1>
+        <p>
+          This extension automatically scrolls to the next video on YouTube or
+          Instagram once the current video ends.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
